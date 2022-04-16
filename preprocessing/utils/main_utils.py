e8736ad828a1200208f281730e6737d36881d0d4
sethusaim <sethusaim@gmail.com>
2022-04-16 12:17:51 +0530

    added minor changes to preprocessing component

--- preprocessing/utils/main_utils.py
+++ preprocessing/utils/main_utils.py
@@ -49,9 +49,9 @@ class Main_Utils:
                 f"Uploaded logs to {self.bucket['input_files']}", self.log_file
             )
 
-            self.log_writer.start_log("exit", self.class_name, method_name, log_file)
+            self.log_writer.start_log("exit", self.class_name, method_name, self.log_file)
 
             rmtree("train_logs")
 
         except Exception as e:
-            self.log_writer.exception_log(e, self.class_name, method_name, log_file)
+            self.log_writer.exception_log(e, self.class_name, method_name, self.log_file)
