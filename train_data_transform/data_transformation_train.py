a8bf0bbcdae3a289000dc0cd3729a230778289dd
sethusaim <sethusaim@gmail.com>
2022-04-16 14:12:25 +0530

    added train data transform component

---
+++ train_data_transform/data_transformation_train.py
@@ -0,0 +1,155 @@
+from s3_operations import S3_Operation
+from utils.logger import App_Logger
+from utils.read_params import read_params
+
+
+class Data_Transform_Train:
+    """
+    Description :   This class shall be used for transforming the good raw training data before loading it in database
+    Written by  :   iNeuron Intelligence
+
+    Version     :   1.2
+    Revisions   :   Moved to setup to cloud 
+    """
+
+    def __init__(self):
+        self.config = read_params()
+
+        self.class_name = self.__class__.__name__
+
+        self.s3 = S3_Operation()
+
+        self.log_writer = App_Logger()
+
+        self.data_dir = self.config["data_dir"]
+
+        self.bucket = self.config["s3_bucket"]
+
+        self.train_data_transform_log = self.config["log"]["data_transform"]
+
+        self.replace_col = self.config["base"]["replace_col"]
+
+        self.target_col = self.config["base"]["target_col"]
+
+    def rename_target_column(self):
+        """
+        Method Name :   rename_target_column
+        Description :   This method renames the target column from Good/Bad to Output.
+                        We are using substring in the first column to keep only "Integer" data for ease up the
+                        loading.This columns is anyways going to be removed during training
+
+        Written by  :   iNeuron Intelligence
+        Revisions   :   moved setup to cloud
+        """
+        method_name = self.rename_target_column.__name__
+
+        self.log_writer.start_log(
+            "start", self.class_name, method_name, self.train_data_transform_log,
+        )
+
+        try:
+            lst = self.s3.read_csv_from_dir(
+                self.good_train_data_dir,
+                self.train_data_bucket,
+                self.train_data_transform_log,
+            )
+
+            lst = self.s3.read_csv_from_dir(
+                self.data_dir["train_good"],
+                self.bucket["train_data"],
+                self.train_data_transform_log,
+            )
+
+            for idx, f in enumerate(lst):
+                df = f[idx][0]
+
+                file = f[idx][1]
+
+                abs_f = f[idx][2]
+
+                if file.endswith(".csv"):
+                    df.rename(columsn={self.replace_col, self.target_col}, inplace=True)
+
+                    self.log_writer.log(
+                        f"Renamed the output columns for the file {file}",
+                        self.train_data_transform_log,
+                    )
+
+                    self.s3.upload_df_as_csv(
+                        df,
+                        abs_f,
+                        file,
+                        self.bucket["input_files"],
+                        self.train_data_transform_log,
+                    )
+
+                else:
+                    pass
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.train_data_transform_log,
+            )
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.train_data_transform_log,
+            )
+
+    def replace_missing_with_null(self):
+        """
+        Method Name :   replace_missing_with_null
+        Description :   This method replaces the missing values in columns with "NULL" to store in the table.
+                        We are using substring in the first column to keep only "Integer" data for ease up the
+                        loading.This columns is anyways going to be removed during training
+        Written by  :   iNeuron Intelligence
+        Revisions   :   moved setup to cloud
+        """
+        method_name = self.replace_missing_with_null.__name__
+
+        self.log_writer.start_log(
+            "start", self.class_name, method_name, self.train_data_transform_log,
+        )
+
+        try:
+            lst = self.s3.read_csv_from_dir(
+                self.data_dir["train_good"],
+                self.bucket["train_data"],
+                self.train_data_transform_log,
+            )
+
+            for idx, f in enumerate(lst):
+                df = f[idx][0]
+
+                file = f[idx][1]
+
+                abs_f = f[idx][2]
+
+                if file.endswith(".csv"):
+                    df.fillna("NULL", inplace=True)
+
+                    df["Wafer"] = df["Wafer"].str[6:]
+
+                    self.log_writer.log(
+                        f"Replaced missing values with null for the file {file}",
+                        self.train_data_transform_log,
+                    )
+
+                    self.s3.upload_df_as_csv(
+                        df,
+                        abs_f,
+                        file,
+                        self.bucket["train_data"],
+                        self.train_data_transform_log,
+                    )
+
+                else:
+                    pass
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.train_data_transform_log,
+            )
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.train_data_transform_log,
+            )
---
+++ train_data_transform/run.py
@@ -0,0 +1,49 @@
+from data_transformation_train import Data_Transform_Train
+from utils.logger import App_Logger
+from utils.main_utils import Main_Utils
+from utils.read_params import read_params
+
+
+class Run:
+    def __init__(self):
+        self.config = read_params()
+
+        self.class_name = self.__class__.__name__
+
+        self.train_main_log = self.config["log"]["train_main"]
+
+        self.log_writer = App_Logger()
+
+        self.data_transform = Data_Transform_Train()
+
+    def train_data_transform(self):
+        method_name = self.train_data_transform.__name__
+
+        try:
+            self.log_writer.log("Starting Data Transformation", self.train_main_log)
+
+            self.data_transform.rename_target_column()
+
+            self.data_transform.replace_missing_with_null()
+
+            self.log_writer.log("Data Transformation completed !!", self.train_main_log)
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.train_main_log,
+            )
+
+
+if __name__ == "__main__":
+    run = Run()
+
+    utils = Main_Utils()
+
+    try:
+        run.train_data_transform()
+
+    except Exception as e:
+        raise e
+
+    finally:
+        utils.upload_logs()
