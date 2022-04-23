39ffeca28961611755e7e77125a2a941f804656a
sethusaim <sethusaim@gmail.com>
2022-04-23 11:28:56 +0530

    code update to preprocessing pred

---
+++ preprocessing_pred/run.py
@@ -0,0 +1,107 @@
+from data_loader_pred import Data_Getter_Pred
+from preprocessing import Preprocessor
+from s3_operations import S3_Operation
+from utils.logger import App_Logger
+from utils.main_utils import Main_Utils
+from utils.read_params import read_params
+
+
+class Run:
+    def __init__(self):
+        self.class_name = self.__class__.__name__
+
+        self.config = read_params()
+
+        self.utils = Main_Utils()
+
+        self.preprocess_log = self.config["log"]["preprocess_pred"]
+
+        self.files = self.config["files"]
+
+        self.bucket = self.config["s3_bucket"]
+
+        self.data_getter_pred = Data_Getter_Pred(self.preprocess_log)
+
+        self.preprocess = Preprocessor(self.preprocess_log)
+
+        self.log_writer = App_Logger()
+
+        self.s3 = S3_Operation()
+
+    def run_preprocess(self):
+        method_name = self.run_preprocess.__name__
+
+        self.log_writer.start_log(
+            "start", self.class_name, method_name, self.preprocess_log
+        )
+
+        try:
+            self.utils.delete_pred_file(self.preprocess_log)
+
+            data = self.data_getter_pred.get_data()
+
+            is_null_present = self.preprocess.is_null_present(data)
+
+            self.log_writer.log(
+                f"Preprocessing function is_null_present returned null values present to be {is_null_present}",
+                self.preprocess_log,
+            )
+
+            self.log_writer.log(
+                "Imputing missing values for the data", self.preprocess_log
+            )
+
+            if is_null_present:
+                data = self.preprocess.impute_missing_values(data)
+
+            self.log_writer.log(
+                "Imputed missing values for the data", self.preprocess_log
+            )
+
+            cols_to_drop = self.preprocess.get_columns_with_zero_std_deviation(data)
+
+            self.log_writer.log(
+                "Got columns with zero standard deviation", self.preprocess_log
+            )
+
+            data = self.preprocess.remove_columns(data, cols_to_drop)
+
+            self.log_writer.log(
+                "Removed columns with zero standard deviation", self.preprocess_log
+            )
+            
+            self.s3.upload_df_as_csv(
+                data,
+                self.files["pred_input_preprocess"],
+                self.files["pred_input_preprocess"],
+                self.bucket["feature_store"],
+                self.preprocess_log
+            )
+
+            self.log_writer.log(
+                "Completed preprocessing for prediction data", self.preprocess_log
+            )
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.preprocess_log
+            )
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.preprocess_log
+            )
+
+
+if __name__ == "__main__":
+    try:
+        run = Run()
+
+        run.run_preprocess()
+
+    except Exception as e:
+        raise e
+
+    finally:
+        utils = Main_Utils()
+
+        utils.upload_logs()
---
+++ preprocessing_pred/utils/main_utils.py
@@ -0,0 +1,147 @@
+from os import listdir
+from os.path import join
+from shutil import rmtree
+
+from boto3 import resource
+from botocore.exceptions import ClientError
+from s3_operations import S3_Operation
+
+from utils.logger import App_Logger
+from utils.read_params import read_params
+
+
+class Main_Utils:
+    def __init__(self):
+        self.s3 = S3_Operation()
+
+        self.log_writer = App_Logger()
+
+        self.config = read_params()
+
+        self.s3_resource = resource("s3")
+
+        self.files = self.config["files"]
+
+        self.bucket = self.config["s3_bucket"]
+
+        self.model_dir = self.config["model_dir"]
+
+        self.log_dir = self.config["log_dir"]
+
+        self.log_file = self.config["log"]["upload"]
+
+        self.class_name = self.__class__.__name__
+
+    def upload_logs(self):
+        method_name = self.upload_logs.__name__
+
+        self.log_writer.start_log("start", self.class_name, method_name, self.log_file)
+
+        try:
+            lst = listdir(self.log_dir)
+
+            self.log_writer.log(
+                f"Got list of logs from {self.log_dir} folder", self.log_file
+            )
+
+            for f in lst:
+                local_f = join(self.log_dir, f)
+
+                dest_f = self.log_dir + "/" + f
+
+                self.s3.upload_file(local_f, dest_f, self.bucket["logs"], self.log_file)
+
+            self.log_writer.log(
+                f"Uploaded logs to {self.bucket['logs']}", self.log_file
+            )
+
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.log_file
+            )
+
+            rmtree(self.log_dir)
+
+        except Exception as e:
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.log_file
+            )
+
+    def find_correct_model_file(self, cluster_number, bucket, log_file):
+        """
+        Method Name :   find_correct_model_file
+        Description :   This method gets correct model file based on cluster number during prediction
+        Output      :   A correct model file is found 
+        On Failure  :   Write an exception log and then raise an exception
+        Version     :   1.2
+        
+        Revisions   :   moved setup to cloud
+        """
+        method_name = self.find_correct_model_file.__name__
+
+        self.log_writer.start_log("start", self.class_name, method_name, log_file)
+
+        try:
+            list_of_files = self.s3.get_files_from_folder(
+                self.model_dir["prod"], bucket, log_file
+            )
+
+            for file in list_of_files:
+                try:
+                    if file.index(str(cluster_number)) != -1:
+                        model_name = file
+
+                except:
+                    continue
+
+            model_name = model_name.split(".")[0]
+
+            self.log_writer.log(
+                f"Got {model_name} from {self.prod_model_dir} folder in {bucket} bucket",
+                log_file,
+            )
+
+            self.log_writer.start_log("exit", self.class_name, method_name, log_file)
+
+            return model_name
+
+        except Exception as e:
+            self.log_writer.exception_log(e, self.class_name, method_name, log_file)
+
+    def delete_pred_file(self, log_file):
+        """
+        Method Name :   delete_pred_file
+        Description :   This method deletes the existing prediction file for the model prediction starts
+        
+        Output      :   An existing prediction file is deleted
+        On Failure  :   Write an exception log and then raise an exception
+        
+        Version     :   1.2
+        Revisions   :   moved setup to cloud
+        """
+        method_name = self.delete_pred_file.__name__
+
+        self.log_writer.start_log("start", self.class_name, method_name, log_file)
+
+        try:
+            self.s3_resource.Object(
+                self.bucket["io_files"], self.files["pred_file"]
+            ).load()
+
+            self.log_writer.log(
+                f"Found existing Prediction batch file. Deleting it.", log_file
+            )
+
+            self.s3.delete_file(
+                self.files["pred_file"], self.bucket["io_files"], log_file
+            )
+
+            self.log_writer.start_log("exit", self.class_name, method_name, log_file)
+
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                pass
+
+            else:
+                self.log_writer.exception_log(
+                    e, self.class_name, method_name, log_file,
+                )
