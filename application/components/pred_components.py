2c1800f8d2c36919ec39d255168133ff945f6259
sethusaim <sethusaim@gmail.com>
2022-04-24 18:47:05 +0530

    added prediction components

--- application/components/pred_components.py
+++ application/components/pred_components.py
@@ -1,72 +1,27 @@
+from distutils.errors import CompileError
+from re import S
+from tkinter import E
 from kfp.dsl import component
 from utils.component_utils import Component
 from utils.logger import App_Logger
 from utils.read_params import read_params
 
 
-class Train_Component:
+class Pred_Component:
     def __init__(self):
         self.config = read_params()
 
         self.bucket = self.config["s3_bucket"]
 
-        self.comp_log = self.config["log"]["train_comp"]
+        self.comp_log = self.config["log"]["pred_comp"]
 
-        self.train_comp = self.config["train_components"]
+        self.pred_comp = self.config["pred_components"]
 
         self.class_name = self.__class__.__name__
 
         self.log_writer = App_Logger()
 
         self.kfp_comp = Component(self.comp_log)
-
-    @component
-    def clustering_component(self):
-        method_name = self.clustering_component.__name__
-
-        self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
-
-        try:
-            comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["clustering"], self.bucket["components"]
-            )
-
-            self.log_writer.log("Got clustering component", self.comp_log)
-
-            self.log_writer.start_log(
-                "exit", self.class_name, method_name, self.comp_log
-            )
-
-            return comp
-
-        except Exception as e:
-            self.log_writer.exception_log(
-                e, self.class_name, method_name, self.comp_log
-            )
-
-    @component()
-    def load_prod_model_component(self):
-        method_name = self.load_prod_model_component.__name__
-
-        self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
-
-        try:
-            comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["load_prod"], self.bucket["components"]
-            )
-
-            self.log_writer.log("Got load production model component", self.comp_log)
-
-            self.log_writer.start_log(
-                "exit", self.class_name, method_name, self.comp_log
-            )
-
-            return comp
-
-        except Exception as e:
-            self.log_writer.exception_log(
-                e, self.class_name, method_name, self.comp_log
-            )
 
     @component
     def preprocessing_component(self):
@@ -76,7 +31,7 @@ class Train_Component:
 
         try:
             comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["preprocess"], self.bucket["components"]
+                self.pred_comp["preprocess"], self.bucket["components"]
             )
 
             self.log_writer.log("Got preprocessing component", self.comp_log)
@@ -91,19 +46,17 @@ class Train_Component:
             )
 
     @component
-    def raw_train_data_val_component(self):
-        method_name = self.raw_train_data_val_component.__name__
+    def pred_db_op_component(self):
+        method_name = self.pred_db_op_component.__name__
 
         self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
 
         try:
             comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["raw_data_val"], self.bucket["components"]
+                self.pred_comp["db_operation"], self.bucket["components"]
             )
 
-            self.log_writer.log(
-                "Got raw train data validation component", self.comp_log
-            )
+            self.log_writer.log("Got pred db operation component", self.comp_log)
 
             self.log_writer.start_log(
                 "exit", self.class_name, method_name, self.comp_log
@@ -117,17 +70,17 @@ class Train_Component:
             )
 
     @component
-    def train_data_trans_component(self):
-        method_name = self.train_data_trans_component.__name__
+    def model_prediction_component(self):
+        method_name = self.model_prediction_component.__name__
 
         self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
 
         try:
             comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["data_trans"], self.bucket["components"]
+                self.pred_comp["model"], self.bucket["components"]
             )
 
-            self.log_writer.log("Got train data transform component", self.comp_log)
+            self.log_writer.log("Got prediction model component", self.comp_log)
 
             self.log_writer.start_log(
                 "exit", self.class_name, method_name, self.comp_log
@@ -141,17 +94,17 @@ class Train_Component:
             )
 
     @component
-    def train_db_op_component(self):
-        method_name = self.train_db_op_component.__name__
+    def preprocessing_pred_component(self):
+        method_name = self.preprocessing_pred_component.__name__
 
         self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
 
         try:
             comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["db_operation"], self.bucket["components"]
+                self.pred_comp["preprocessing"], self.bucket["components"]
             )
 
-            self.log_writer.log("Got train db operation component", self.comp_log)
+            self.log_writer.log("Got preprocessing prediction component", self.comp_log)
 
             self.log_writer.start_log(
                 "exit", self.class_name, method_name, self.comp_log
@@ -165,17 +118,17 @@ class Train_Component:
             )
 
     @component
-    def training_model_component(self):
-        method_name = self.training_model_component.__name__
+    def raw_pred_data_validation_component(self):
+        method_name = self.raw_pred_data_validation_component.__name__
 
         self.log_writer.start_log("start", self.class_name, method_name, self.comp_log)
 
         try:
             comp = self.kfp_comp.load_kfp_component(
-                self.train_comp["model"], self.bucket["components"]
+                self.pred_comp["raw_data_val"], self.bucket["components"]
             )
 
-            self.log_writer.log("Got training model component", self.comp_log)
+            self.log_writer.log("Got raw_pred_data_validation_component", self.comp_log)
 
             self.log_writer.start_log(
                 "exit", self.class_name, method_name, self.comp_log
