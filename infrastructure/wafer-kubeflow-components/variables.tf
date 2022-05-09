c28b99fe14256a2f2b5b705d957a07a911ea5ced
sethusaim <sethusaim@gmail.com>
2022-05-09 22:54:24 +0530

    added terraform script for kubeflow components

---
+++ infrastructure/wafer-kubeflow-components/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "kubeflow_components" {
+  type    = string
+  default = "wafer-kubeflow-components-4efdbdc"
+}
+
+variable "aws_account_id" {
+  type    = string
+  default = "347460842118"
+}
