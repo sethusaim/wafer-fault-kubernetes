21e9e3bc27831f5bced4e4732c5b304fe2390b31
sethusaim <sethusaim@gmail.com>
2022-05-09 23:17:00 +0530

    code fix in module.tf

--- infrastructure/module.tf
+++ infrastructure/module.tf
@@ -1,43 +1,43 @@
-module "jenkins" {
+module "jenkins_instance" {
   source = "./jenkins"
 }
 
-module "mlflow" {
-  source = "./mlflow"
-}
-
-module "feature_store" {
-  source = "./wafer-feature-store"
-}
-
-module "io_files" {
-  source = "./wafer-io-files"
-}
-
-module "kubeflow_components" {
-  source = "./wafer-kubeflow-components"
-}
-
-module "logs" {
-  source = "./wafer-logs"
-}
-
-module "mlflow" {
+module "mlflow_instance" {
   source = "./wafer-mlflow"
 }
 
-module "model" {
+module "feature_store_bucket" {
+  source = "./wafer-feature-store"
+}
+
+module "io_files_bucket" {
+  source = "./wafer-io-files"
+}
+
+module "kubeflow_components_bucket" {
+  source = "./wafer-kubeflow-components"
+}
+
+module "logs_bucket" {
+  source = "./wafer-logs"
+}
+
+module "mlflow_bucket" {
+  source = "./wafer-mlflow"
+}
+
+module "model_bucket" {
   source = "./wafer-model"
 }
 
-module "pred-data" {
+module "pred-data_bucket" {
   source = "./wafer-pred-data"
 }
 
-module "raw-data" {
-  source = "./wafe-raw-data"
+module "raw-data_bucket" {
+  source = "./wafer-raw-data"
 }
 
-module "train-data" {
+module "train-data_bucket" {
   source = "./wafer-train-data"
 }
