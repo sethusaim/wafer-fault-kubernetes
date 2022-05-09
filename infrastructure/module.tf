407580dd165dd67a1fb797eb7a769dd15dec7898
sethusaim <sethusaim@gmail.com>
2022-05-09 23:10:18 +0530

    added modules for all s3 buckets

--- infrastructure/module.tf
+++ infrastructure/module.tf
@@ -15,9 +15,29 @@ module "io_files" {
 }
 
 module "kubeflow_components" {
-  source = "./wafer-kubeflow-components" 
+  source = "./wafer-kubeflow-components"
 }
 
 module "logs" {
   source = "./wafer-logs"
+}
+
+module "mlflow" {
+  source = "./wafer-mlflow"
+}
+
+module "model" {
+  source = "./wafer-model"
+}
+
+module "pred-data" {
+  source = "./wafer-pred-data"
+}
+
+module "raw-data" {
+  source = "./wafe-raw-data"
+}
+
+module "train-data" {
+  source = "./wafer-train-data"
 }
