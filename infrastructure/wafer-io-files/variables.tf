3b405e3c7d9c7bc622dee2598667d5582ce73d7b
sethusaim <sethusaim@gmail.com>
2022-05-09 22:52:29 +0530

    added terraform script for io files bucket

---
+++ infrastructure/wafer-io-files/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "io_files_bucket" {
+  type    = string
+  default = "wafer-io-files-03e0100"
+}
+
+variable "aws_account_id" {
+  type    = string
+  default = "347460842118"
+}
