d3b711d2c1ac7fb99fa5148186e2af186e97082a
sethusaim <sethusaim@gmail.com>
2022-05-14 12:30:11 +0530

    added code changes to infrastructure

--- infrastructure/module.tf
+++ infrastructure/module.tf
@@ -1,47 +1,52 @@
-# module "jenkins_instance" {
-#   source = "./jenkins_instance"
-# }
+module "jenkins_instance" {
+  source = "./jenkins_instance"
+}
 
 module "mlflow_instance" {
   source = "./mlflow_instance"
 }
 
-# module "feature_store_bucket" {
-#   source = "./wafer_feature_store_bucket"
-# }
+module "eks_cluster" {
+  source = "./wafer_eks_cluster"
+}
 
-# module "io_files_bucket" {
-#   source = "./wafer_io_files_bucket"
-# }
+module "feature_store_bucket" {
+  source = "./wafer_feature_store_bucket"
+}
 
-# module "kube_master" {
-#   source = "./wafer_kube_master"
-# }
+module "io_files_bucket" {
+  source = "./wafer_io_files_bucket"
+}
 
-# module "kubeflow_components_bucket" {
-#   source = "./wafer_kubeflow_components_bucket"
-# }
+module "kube_master" {
+  source = "./wafer_kube_master"
+}
 
-# module "logs_bucket" {
-#   source = "./wafer_logs_bucket"
-# }
+module "kubeflow_components_bucket" {
+  source = "./wafer_kubeflow_components_bucket"
+}
 
-# module "mlflow_bucket" {
-#   source = "./wafer_mlflow_bucket"
-# }
+module "logs_bucket" {
+  source = "./wafer_logs_bucket"
+}
 
-# module "model_bucket" {
-#   source = "./wafer_model_bucket"
-# }
+module "mlflow_bucket" {
+  source = "./wafer_mlflow_bucket"
+}
 
-# module "pred_data_bucket" {
-#   source = "./wafer_pred_data_bucket"
-# }
+module "model_bucket" {
+  source = "./wafer_model_bucket"
+}
 
-# module "raw_data_bucket" {
-#   source = "./wafer_raw_data_bucket"
-# }
+module "pred_data_bucket" {
+  source = "./wafer_pred_data_bucket"
+}
 
-# module "train_data_bucket" {
-#   source = "./wafer_train_data_bucket"
-# }
+module "raw_data_bucket" {
+  source = "./wafer_raw_data_bucket"
+}
+
+module "train_data_bucket" {
+  source = "./wafer_train_data_bucket"
+}
+
---
+++ infrastructure/wafer_eks_cluster/variables.tf
@@ -0,0 +1,90 @@
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
+
+variable "wafer_sg_group_name" {
+  default = "terraform-eks-wafer-cluster"
+  type    = string
+}
+
+variable "wafer_sg_group_egress_from_port" {
+  default = 0
+  type    = number
+}
+
+variable "wafer_sg_group_egress_to_port" {
+  default = 0
+  type    = number
+}
+
+variable "wafer_sg_group_protocol" {
+  default = "-1"
+  type    = string
+}
+
+variable "wafer_sg_group_cidr_block" {
+  default = ["0.0.0.0/0"]
+  type    = list(string)
+}
+
+variable "wafer_sg_group_tag_name" {
+  default = "terraform-eks-wafer"
+  type    = string
+}
+
+variable "wafer_node_iam_role_name" {
+  default = "terraform-eks-wafer-node"
+  type    = string
+}
+
+variable "eks_node_group_name" {
+  default = "wafer"
+  type    = string
+}
+
+variable "desired_node_size" {
+  default = 2
+  type    = number
+}
+
+variable "min_node_size" {
+  default = 1
+  type    = number
+}
+
+variable "max_node_size" {
+  default = 3
+  type    = number
+}
+
+variable "cluster_vpc_cidr_block" {
+  default = "10.0.0.0/16"
+  type    = string
+}
+
+variable "cluster_vpc_tag_name" {
+  default = "terraform-eks-wafer-node"
+  type    = string
+}
+
+variable "cluster_subnet_tag_name" {
+  default = "terraform-eks-wafer-node"
+  type    = string
+}
+
+variable "cluster_internet_gateway_tag_name" {
+  default = "terraform-eks-wafer"
+  type    = string
+}
+
+variable "route_table_cidr_block" {
+  default = "0.0.0.0/0"
+  type    = string
+}
+
