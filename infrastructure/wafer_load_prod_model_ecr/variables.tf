72362a90d8d56b60a98d7c2d8ea4c891f3ceb1c3
sethusaim <sethusaim@gmail.com>
2022-05-15 22:07:08 +0530

    added load prod model ecr repo

---
+++ infrastructure/wafer_load_prod_model_ecr/variables.tf
@@ -0,0 +1,14 @@
+variable "wafer_load_prod_model_ecr_name" {
+  default = "wafer_load_prod_models"
+  type    = string
+}
+
+variable "image_tag_mutability" {
+  default = "MUTABLE"
+  type    = string
+}
+
+variable "scan_on_push" {
+  default = true
+  type    = bool
+}
