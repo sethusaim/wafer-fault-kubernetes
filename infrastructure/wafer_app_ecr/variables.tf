1a3014c0e243d0c5499d2d2bda68e02949b84520
sethusaim <sethusaim@gmail.com>
2022-05-21 15:43:28 +0530

    added application ecr repo to infrastructure

---
+++ infrastructure/wafer_app_ecr/variables.tf
@@ -0,0 +1,14 @@
+variable "wafer_app_ecr_name" {
+  default = "wafer_application"
+  type    = string
+}
+
+variable "image_tag_mutability" {
+  default = "MUTABLE"
+  type    = string
+}
+
+variable "scan_on_push" {
+  default = true
+  type    = bool
+}
