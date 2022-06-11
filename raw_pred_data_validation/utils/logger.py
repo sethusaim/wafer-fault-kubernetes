7037f7f806ae546e1c665595556d3c40cefdccdc
sethusaim <sethusaim@gmail.com>
2022-06-11 17:22:26 +0530

    added python logger

--- raw_pred_data_validation/utils/logger.py
+++ raw_pred_data_validation/utils/logger.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from logging import DEBUG, ERROR, basicConfig, error, info
 from os import makedirs
 from os.path import join
 
@@ -6,43 +7,16 @@ from utils.read_params import read_params
 
 
 class App_Logger:
-    """
-    Description :   This class is used for logging the information to text files
-    Version     :   1.2
-
-    Revisions   :   Moved to setup to cloud 
-    """
-
     def __init__(self):
-        self.class_name = self.__class__.__name__
-
         self.config = read_params()
 
         self.log_dir = self.config["log_dir"]
 
+        self.current_date = f"{datetime.now().strftime('%Y-%m-%d')}"
+
         makedirs(self.log_dir, exist_ok=True)
 
-    def write_info_to_file(self, log_info, log_file):
-        """
-        Method Name :   write_info_to_file
-        Description :   This method writes the logging information to text file
-        
-        Output      :   The logging information is successfully written to text file.
-        On Failure  :   Write an exception log and then raise an exception
-        
-        Version     :   1.2
-        Revisions   :   moved setup to cloud
-        """
-        try:
-            with open(log_file, "a+") as f:
-                f.write(log_info)
-
-                f.close()
-
-        except Exception as e:
-            raise e
-
-    def log(self, log_info, log_file):
+    def log(self, log_message, log_file):
         """
         Method Name :   log
         Description :   This method writes the log info using current date and time
@@ -54,19 +28,19 @@ class App_Logger:
         Revisions   :   moved setup to cloud
         """
         try:
-            self.now = datetime.now()
+            log_f = self.current_date + "-" + log_file
 
-            self.date = self.now.date().strftime("%d-%m-%Y")
+            log_file = join(self.log_dir, log_f)
 
-            self.current_time = self.now.strftime("%H:%M:%S")
-
-            log_fpath = join(self.log_dir, log_file)
-
-            log_msg = (
-                str(self.date) + "  " + str(self.current_time) + "  " + log_info + "\n"
+            basicConfig(
+                filename=log_file,
+                filemode="a",
+                format="%(asctime)s %(levelname)s %(message)s",
+                datefmt="%H:%M:%S",
+                level=DEBUG,
             )
 
-            self.write_info_to_file(log_msg, log_fpath)
+            info(log_message)
 
         except Exception as e:
             raise e
@@ -93,7 +67,7 @@ class App_Logger:
             self.log(log_msg, log_file)
 
         except Exception as e:
-            error_msg = f"Exception occured in Class : {self.class_name}, Method : {start_method_name}, Error : {str(e)}"
+            error_msg = f"Exception occured in Class : {class_name}, Method : {start_method_name}, Error : {str(e)}"
 
             raise Exception(error_msg)
 
@@ -110,8 +84,19 @@ class App_Logger:
         """
         exception_msg = f"Exception occured in Class : {class_name}, Method : {method_name}, Error : {str(exception)}"
 
-        self.log(exception_msg, log_file)
+        log_f = self.current_date + "-" + log_file
 
-        self.start_log("exit", class_name, method_name, log_file)
+        log_file = join(self.log_dir, log_f)
+
+        basicConfig(
+            filename=log_file,
+            filemode="a",
+            format="%(asctime)s %(levelname)s %(message)s",
+            datefmt="%H:%M:%S",
+            level=ERROR,
+        )
+
+        error(exception_msg)
 
         raise Exception(exception_msg)
+s
