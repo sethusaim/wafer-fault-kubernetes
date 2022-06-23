e282ff188e914ab9e482d7181f3835857e1d554b
sethusaim <sethusaim@gmail.com>
2022-06-23 13:36:37 +0530

    minor fix

--- model_training/tuner.py
+++ model_training/tuner.py
@@ -214,7 +214,7 @@ class Model_Finder:
                 e, self.class_name, method_name, self.log_file
             )
 
-    def train_and_log_models(self, X_data, Y_data, log_file, idx):
+    def train_and_log_models(self, X_data, Y_data, idx):
         """
         Method Name :   train_and_log_models
         Description :   This methods trains all the models based on train data and used mlflow to log all the models
@@ -227,7 +227,7 @@ class Model_Finder:
         """
         method_name = self.train_and_log_models.__name__
 
-        self.log_writer.start_log("start", self.class_name, method_name, log_file)
+        self.log_writer.start_log("start", self.class_name, method_name, self.log_file)
 
         try:
             x_train, x_test, y_train, y_test = train_test_split(
@@ -236,39 +236,45 @@ class Model_Finder:
 
             self.log_writer.log(
                 f"Performed train test split with kwargs as {self.split_kwargs}",
-                log_file,
+                self.log_file,
             )
 
             lst = self.get_trained_models(x_train, y_train, x_test, y_test)
 
-            self.log_writer.log("Got trained models", log_file)
+            self.log_writer.log("Got trained models", self.log_file)
 
             for _, tm in enumerate(lst):
                 model = tm[0]
 
                 model_score = tm[1]
 
-                self.s3.save_model(model, "train_model", "model", log_file, idx=idx)
+                self.s3.save_model(
+                    model, "train_model", "model", self.log_file, idx=idx
+                )
 
                 self.mlflow_op.log_all_for_model(model, model_score, idx)
 
             self.log_writer.log(
-                "Saved and logged all trained models to mlflow", log_file
+                "Saved and logged all trained models to mlflow", self.log_file
             )
 
-            self.log_writer.start_log("exit", self.class_name, method_name, log_file)
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.log_file
+            )
 
         except Exception as e:
-            self.log_writer.exception_log(e, self.class_name, method_name, log_file)
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.log_file
+            )
 
-    def perform_training(self, lst_clusters, log_file):
+    def perform_training(self, lst_clusters):
         method_name = self.perform_training.__name__
 
-        self.log_writer.start_log("start", self.class_name, method_name, log_file)
+        self.log_writer.start_log("start", self.class_name, method_name, self.log_file)
 
         try:
             kmeans_model = self.s3.load_model(
-                "KMeans", "model", log_file, model_dir="train_model"
+                "KMeans", "model", self.log_file, model_dir="train_model"
             )
 
             kmeans_model_name = kmeans_model.__class__.__name__
@@ -283,18 +289,22 @@ class Model_Finder:
                 end_run()
 
             for i in range(lst_clusters):
-                cluster_feat = self.utils.get_cluster_features(i, log_file)
+                cluster_feat = self.utils.get_cluster_features(i, self.log_file)
 
-                cluster_label = self.utils.get_cluster_targets(i, log_file)
+                cluster_label = self.utils.get_cluster_targets(i, self.log_file)
 
-                self.log_writer.log("Got cluster features and cluster labels", log_file)
+                self.log_writer.log(
+                    "Got cluster features and cluster labels", self.log_file
+                )
 
                 with start_run(run_name=self.mlflow_config["run_name"] + str(i)):
-                    self.train_and_log_models(
-                        cluster_feat, cluster_label, log_file, idx=i,
-                    )
+                    self.train_and_log_models(cluster_feat, cluster_label, idx=i)
 
-            self.log_writer.start_log("exit", self.class_name, method_name, log_file)
+            self.log_writer.start_log(
+                "exit", self.class_name, method_name, self.log_file
+            )
 
         except Exception as e:
-            self.log_writer.exception_log(e, self.class_name, method_name, log_file)
+            self.log_writer.exception_log(
+                e, self.class_name, method_name, self.log_file
+            )
