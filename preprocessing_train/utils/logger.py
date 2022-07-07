43b5fc3953911bf2f171c94ec915e561ea3ad2bb
sethusaim <sethusaim@gmail.com>
2022-07-07 13:19:12 +0530

    improvised logger

--- preprocessing_train/utils/logger.py
+++ preprocessing_train/utils/logger.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from logging import DEBUG, ERROR, basicConfig, error, info
 from os import makedirs
-from os.path import join
+from os.path import join, split
+from sys import exc_info
 
 from utils.read_params import read_params
 
@@ -12,11 +13,27 @@ class App_Logger:
 
         self.log_dir = self.config["dir"]["log"]
 
-        self.log_file = self.config["log"]
-
         self.current_date = f"{datetime.now().strftime('%Y-%m-%d')}"
 
+        self.log_file = self.config["log"]
+
         makedirs(self.log_dir, exist_ok=True)
+
+    def get_log_file(self, log_file):
+        """
+        Method Name :   get_log_file
+        Description :   This method gets the log file with path from the log_file key
+        
+        Output      :   The log file with path is returned 
+        
+        Version     :   1.2
+        Revisions   :   moved setup to cloud
+        """
+        log_f = self.current_date + "-" + self.log_file[log_file]
+
+        log_file = join(self.log_dir, log_f)
+
+        return log_file
 
     def log(self, log_message, log_file):
         """
@@ -30,9 +47,7 @@ class App_Logger:
         Revisions   :   moved setup to cloud
         """
         try:
-            log_f = self.current_date + "-" + self.log_file[log_file]
-
-            log_file = join(self.log_dir, log_f)
+            log_file = self.get_log_file(log_file)
 
             basicConfig(
                 filename=log_file,
@@ -84,11 +99,13 @@ class App_Logger:
         Version     :   1.2
         Revisions   :   moved setup to cloud
         """
-        exception_msg = f"Exception occured in Class : {class_name}, Method : {method_name}, Error : {str(exception)}"
+        _, _, exc_tb = exc_info()
 
-        log_f = self.current_date + "-" + log_file
+        file_name = split(exc_tb.tb_frame.f_code.co_filename)[1]
 
-        log_file = join(self.log_dir, log_f)
+        exception_msg = f"- Class : {class_name}, Method : {method_name}, Script : {file_name}, Line : {exc_tb.tb_lineno}, Error : {str(exception)}"
+
+        log_file = self.get_log_file(log_file)
 
         basicConfig(
             filename=log_file,
