a3a2f6c7de79f566f1453d4b5233f24cd8525dd3
sethusaim <sethusaim@gmail.com>
2022-07-10 18:24:06 +0530

    minor code fix

--- preprocessing_pred/data_loader_pred.py
+++ preprocessing_pred/data_loader_pred.py
@@ -1,11 +1,12 @@
 from s3_operations import S3_Operation
 from utils.logger import App_Logger
+from utils.main_utils import Main_Utils
 from utils.read_params import get_log_dic
 
 
 class Data_Getter_Pred:
     """
-    Description :   This class shall be used for obtaining the df from the input files s3 bucket where the prediction file is present
+    Description :   This class shall be used for obtaining the df from the input files s3 bucket where the training file is present
     Version     :   1.2
     
     Revisions   :   Moved to setup to cloud 
@@ -14,6 +15,8 @@ class Data_Getter_Pred:
     def __init__(self, log_file):
         self.log_file = log_file
 
+        self.utils = Main_Utils()
+
         self.s3 = S3_Operation()
 
         self.log_writer = App_Logger()
@@ -21,10 +24,10 @@ class Data_Getter_Pred:
     def get_data(self):
         """
         Method Name :   get_data
-        Description :   This method reads the data from the input files s3 bucket where the prediction file is present
+        Description :   This method reads the data from the input files s3 bucket where the training file is stored
         
         Output      :   A pandas dataframe
-        On Failure  :   Write an exception log and then raise an exception    
+        On Failure  :   Write an exception log and then raise exception    
         
         Version     :   1.2
         Revisions   :   moved setup to cloud
@@ -36,10 +39,16 @@ class Data_Getter_Pred:
         self.log_writer.start_log("start", **log_dic)
 
         try:
-            df = self.s3.read_csv("pred_input", "feature_store", log_dic["log_file"])
+            ip_fname = self.utils.get_file_with_timestamp(
+                "pred_export", log_dic["log_file"]
+            )
+
+            df = self.s3.read_csv(
+                ip_fname, "feature_store", log_dic["log_file"], fidx=True
+            )
 
             self.log_writer.log(
-                "Data loaded from pred_input file and feature store bucket", **log_dic
+                "Prediction data loaded from feature store bucket", **log_dic
             )
 
             self.log_writer.start_log("exit", **log_dic)
