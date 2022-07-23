bb31f26977ae303c0b2b215b8d34675598fffcc1
sethusaim <sethusaim@gmail.com>
2022-07-23 18:00:21 +0530

    made changes to run ansbile script based on folder changes

--- scripts/run_ansible.sh
+++ scripts/run_ansible.sh
@@ -8,8 +8,8 @@ git remote add origin https://github.com/sethusaim/Ansible-AWS-Demo.git
 
 git fetch origin
 
-git checkout origin/main -- playbooks
+git checkout origin/main -- ansible_playbooks
 
-git checkout origin/main -- vars
+cd ansible_playbooks
 
 sudo ansible-playbook playbooks/main.yml
