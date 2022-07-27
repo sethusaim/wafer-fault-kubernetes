bcdd505393c69e6d4c966c34a125e1e65933437b
sethusaim <sethusaim@gmail.com>
2022-07-27 15:01:57 +0530

    restructured model bucket

--- infrastructure/s3_buckets/wafer_model_bucket/model.tf
+++ infrastructure/s3_buckets/wafer_model_bucket/model.tf
@@ -1,25 +1,3 @@
 resource "aws_s3_bucket" "model" {
   bucket = var.model
 }
-
-resource "aws_s3_bucket_policy" "allow_full_access" {
-  bucket = aws_s3_bucket.model.id
-  policy = data.aws_iam_policy_document.allow_full_access.json
-}
-
-
-data "aws_iam_policy_document" "allow_full_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [var.aws_account_id]
-    }
-
-    actions = ["s3:*"]
-
-    resources = [
-      aws_s3_bucket.model.arn,
-      "${aws_s3_bucket.model.arn}/*",
-    ]
-  }
-}
