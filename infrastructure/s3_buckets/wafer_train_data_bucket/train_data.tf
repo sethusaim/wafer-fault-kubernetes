1951df1a2c3e5466ac319f6da5b192d84e1a2dc5
sethusaim <sethusaim@gmail.com>
2022-07-27 15:05:40 +0530

    restructured train data bucket

--- infrastructure/s3_buckets/wafer_train_data_bucket/train_data.tf
+++ infrastructure/s3_buckets/wafer_train_data_bucket/train_data.tf
@@ -1,25 +1,3 @@
 resource "aws_s3_bucket" "train-data" {
   bucket = var.train-data
 }
-
-resource "aws_s3_bucket_policy" "allow_full_access" {
-  bucket = aws_s3_bucket.train-data.id
-  policy = data.aws_iam_policy_document.allow_full_access.json
-}
-
-
-data "aws_iam_policy_document" "allow_full_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = [var.aws_account_id]
-    }
-
-    actions = ["s3:*"]
-
-    resources = [
-      aws_s3_bucket.train-data.arn,
-      "${aws_s3_bucket.train-data.arn}/*",
-    ]
-  }
-}
