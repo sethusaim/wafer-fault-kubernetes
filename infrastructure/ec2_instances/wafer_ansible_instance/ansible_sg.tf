b79640883a76a949e80c0f04b4a7a35a026733ba
sethusaim <sethusaim@gmail.com>
2022-08-02 10:47:42 +0530

    formatted ansible_sg.tf

--- infrastructure/ec2_instances/wafer_ansible_instance/ansible_sg.tf
+++ infrastructure/ec2_instances/wafer_ansible_instance/ansible_sg.tf
@@ -8,21 +8,21 @@ resource "aws_security_group" "security_group" {
     protocol    = var.ansible_protocol
     cidr_blocks = var.ansible_cidr_block
   }
-  
+
   ingress {
     from_port   = var.ansible_ingress_from_port[1]
     to_port     = var.ansible_ingress_to_port[1]
     protocol    = var.ansible_protocol
     cidr_blocks = var.ansible_cidr_block
-  }  
-  
+  }
+
   ingress {
     from_port   = var.ansible_ingress_from_port[2]
     to_port     = var.ansible_ingress_to_port[2]
     protocol    = var.ansible_protocol
     cidr_blocks = var.ansible_cidr_block
   }
-  
+
   egress {
     from_port   = var.ansible_egress_from_port
     to_port     = var.ansible_egress_to_port
