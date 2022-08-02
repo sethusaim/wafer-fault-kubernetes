5ee8beb8af10fc6f286ddf0874a12d8082e5cea1
sethusaim <sethusaim@gmail.com>
2022-08-02 15:05:11 +0530

    added setup_ansible.sh

---
+++ scripts/setup_ansible.sh
@@ -0,0 +1,55 @@
+#!bin/bash
+
+sudo apt update
+
+sudo apt-get update
+
+echo "Installing Terraform"
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update && sudo apt-get install terraform
+
+echo "Installed Terraform"
+
+echo "Installing Ansible"
+
+sudo apt update
+
+sudo apt-get update
+
+sudo apt-add-repository -y ppa:ansible/ansible
+
+sudo apt-get update
+
+sudo apt-get install -y ansible
+
+sudo apt install -y python3-pip
+
+pip3 install boto3
+
+ansible --version
+
+sudo apt install awscli
+
+pip3 install awscli
+
+sudo rm install_ansible.sh 
+
+echo "Installed Ansible"
+
+echo "Setting up git repo"
+
+git init 
+
+git remote add origin https://github.com/sethusaim/Wafer-Fault-Kubernetes.git
+
+git checkout origin/main -- infrastructure
+
+git checkout origin/main -- ansible-playbooks
+
+echo "Git repo setup is done"
