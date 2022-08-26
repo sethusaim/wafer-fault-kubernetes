931605c3c574647716d11f02906d0d32a67a5a28
sethusaim <sethusaim@gmail.com>
2022-08-26 10:33:56 +0530

    improvised kubectl delete cron job

--- scripts/kubectl_delete_cron_job.sh
+++ scripts/kubectl_delete_cron_job.sh
@@ -10,8 +10,4 @@ kubectl delete --all pods
 
 echo "Deleted all pods at $currentDate" >> "${job_log_file}"
 
-kubectl delete --all pipelineruns
-
-echo "Deleted all pipeline runs at $currentDate" >> "${job_log_file}"
-
 echo "Exited cron job execution script at $currentDate" >> "${job_log_file}
