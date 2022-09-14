047f27bcb9c10038864851125dc266d393f324c7
sethusaim <sethusaim@gmail.com>
2022-09-14 14:19:24 +0530

    changed git url

--- scripts/run_ansible.sh
+++ scripts/run_ansible.sh
@@ -4,7 +4,7 @@ git init
 
 git remote remove origin
 
-git remote add origin https://github.com/sethusaim/Ansible-AWS-Demo.git
+git remote add origin https://github.com/sethusaim/Wafer-Fault-Kubernetes.git
 
 git fetch origin
 
