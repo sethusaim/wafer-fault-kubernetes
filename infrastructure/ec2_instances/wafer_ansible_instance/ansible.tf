8aa8fdc1474944bbd4773e52da90c9d31b697f3c
sethusaim <sethusaim@gmail.com>
2022-09-14 14:19:52 +0530

    added remote exec for setting up ansible

--- infrastructure/ec2_instances/wafer_ansible_instance/ansible.tf
+++ infrastructure/ec2_instances/wafer_ansible_instance/ansible.tf
@@ -7,6 +7,22 @@ resource "aws_instance" "ansible_instance" {
   instance_type          = var.ansible_instance_type
   key_name               = var.ansible_key_pair_name
   vpc_security_group_ids = [aws_security_group.security_group.id]
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget https://raw.githubusercontent.com/sethusaim/Wafer-Fault-Kubernetes/main/scripts/setup_ansible.sh",
+
+      "bash setup_ansible.sh"
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      private_key = file("C:/Users/sethu/Downloads/sethusaim.pem")
+      host = self.public_ip
+    }
+  }
+
   tags = {
     Name = var.ansible_tag_name
   }
