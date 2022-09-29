db5805e5ee61a425133c1c439f9e5fa2639a1e4a
sethusaim <sethusaim@gmail.com>
2022-09-29 20:23:43 +0530

    formatted code

--- preprocessing_pred/run.py
+++ preprocessing_pred/run.py
@@ -79,4 +79,3 @@ if __name__ == "__main__":
 
     except Exception as e:
         raise e
-
--- preprocessing_pred/wafer_preprocess_pred/__init__.py
+++ preprocessing_pred/wafer_preprocess_pred/__init__.py
@@ -4,7 +4,10 @@ import os
 from from_root import from_root
 
 logs_path = os.path.join(
-    from_root(), "preprocessing_pred", "wafer_preprocess_pred", "preprocesssing_pred_logs"
+    from_root(),
+    "preprocessing_pred",
+    "wafer_preprocess_pred",
+    "preprocesssing_pred_logs",
 )
 
 os.makedirs(logs_path, exist_ok=True)
