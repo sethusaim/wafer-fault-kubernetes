ce0b5ad3e7374bfce601a25581f9dbd5989a7279
sethusaim <sethusaim@gmail.com>
2022-09-30 11:20:04 +0530

    minor fixes

--- raw_train_data_validation/wafer_raw_val/__init__.py
+++ raw_train_data_validation/wafer_raw_val/__init__.py
@@ -4,11 +4,7 @@ import os
 from from_root import from_root
 
 logs_path = os.path.join(
-<<<<<<< HEAD:preprocessing_pred/__init__.py
-    from_root(), "wafer_preprocesssing_pred", "wafer_preprocess_pred", "logs"
-=======
     from_root(), "raw_train_data_validation", "wafer_raw_val", "wafer_raw_val_logs"
->>>>>>> 9a49ca66aedf49b9aa306b47001004e3aaa9192a:raw_train_data_validation/wafer_raw_val/__init__.py
 )
 
 os.makedirs(logs_path, exist_ok=True)
